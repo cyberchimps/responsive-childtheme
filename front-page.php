668f7d52f74748d949268aaa8585c728bcde67ff
Ulrich Pogson <grapplerulrich@gmail.com>
2013-03-23 22:00:12 +0100

    Full Featured - Initial Commit

---
+++ front-page.php
@@ -0,0 +1,56 @@
+<?php
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+
+/**
+ * Site Front Page
+ *
+ * Note: You can overwrite front-page.php as well as any other Template in Child Theme.
+ * Create the same file (name) include in /responsive-child-theme/ and you're all set to go!
+ * @see            http://codex.wordpress.org/Child_Themes and
+ *                 http://themeid.com/forum/topic/505/child-theme-example/
+ *
+ * @file           front-page.php
+ * @package        Responsive 
+ * @author         Emil Uzelac 
+ * @copyright      2003 - 2013 ThemeID
+ * @license        license.txt
+ * @version        Release: 1.0
+ * @filesource     wp-content/themes/responsive/front-page.php
+ * @link           http://codex.wordpress.org/Template_Hierarchy
+ * @since          available since Release 1.0
+ */
+
+/**
+ * Globalize Theme Options
+ */
+global $responsive_options;
+$responsive_options = responsive_get_options();
+
+/**
+ * If front page is set to display the
+ * blog posts index, include home.php;
+ * otherwise, display static front page
+ * content
+ */
+if ( 'posts' == get_option( 'show_on_front' ) ) {
+	get_template_part( 'home' );
+} else if ( 'default' != get_post_meta( get_option( 'page_on_front' ), '_wp_page_template', true ) ) {
+	locate_template( get_post_meta( get_option( 'page_on_front' ), '_wp_page_template', true ), true );
+} else { 
+
+	get_header(); 
+	?>
+
+	<div id="featured" class="grid col-940">
+
+			<?php echo do_shortcode( $responsive_options['featured_content'] ); ?>
+
+	</div><!-- end of #featured -->
+
+	<?php 
+	get_sidebar('home');
+	get_footer(); 
+}
+?>
